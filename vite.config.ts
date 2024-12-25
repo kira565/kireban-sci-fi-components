e3044ad6676f435370b8903e4fa3e73730184a6e
Kiriill Kagdin <rlyiamberserker@gmail.com>
2024-12-25 19:40:26 +0200

    fx

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
 import svgr from 'vite-plugin-svgr';
@@ -24,5 +23,5 @@ export default defineConfig({
     sourcemap: true, // Generates source maps for debugging.
     emptyOutDir: true // Clears the output directory before building.
   },
-  plugins: [svgr(), react(), dts({ rollupTypes: true }), cssInjectedByJsPlugin()]
+  plugins: [svgr(), react(), dts({ rollupTypes: true })]
 });
