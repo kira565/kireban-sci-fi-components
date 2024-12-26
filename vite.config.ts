25694a6f18de38bce5d4fd0d4947dd807557d9a8
Kiriill Kagdin <rlyiamberserker@gmail.com>
2024-12-26 02:08:52 +0200

    bi4

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
       fileName: (format) => `index.${format}.js` // Generates the output file name based on the format.
     },
     rollupOptions: {
-      external: ['react', 'react-dom', 'tailwindcss'], // Defines external dependencies for Rollup bundling.
+      external: ['react', 'react-dom'], // Defines external dependencies for Rollup bundling.
       output: {
         globals: {
           react: 'React',
