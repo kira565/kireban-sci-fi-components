816efc6dbebc947f52b1d053fedc355b7da11c79
Kiriill Kagdin <rlyiamberserker@gmail.com>
2025-01-23 16:58:44 +0200

    svg animation work

--- lib/components/FingerPrintButton/FingerPrintButton.stories.tsx
+++ lib/components/FingerPrintButton/FingerPrintButton.stories.tsx
@@ -10,4 +10,4 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = { args: { width: 80, height: 80 } };
+export const Default: Story = { args: { width: 150, height: 150 } };
--- lib/components/FingerPrintButton/FingerPrintButton.tsx
+++ lib/components/FingerPrintButton/FingerPrintButton.tsx
@@ -1,4 +1,6 @@
-import FingerPrintSvg from '../../assets/svg/fingerprint-svgrepo-com.svg?react';
+import { useRef } from 'react';
+import FingerPrintSvg from '../../assets/svg/fgp.svg?react';
+import { gsap } from 'gsap';
 
 export interface FingerPrintButtonProps {
   width: number;
@@ -6,11 +8,34 @@ export interface FingerPrintButtonProps {
 }
 
 export const FingerPrintButton: React.FC<FingerPrintButtonProps> = ({ width, height }) => {
+  const svgRef = useRef<SVGSVGElement>(null);
+  const animationRef = useRef<gsap.core.Tween>(null);
+
+  const handleMouseEnter = () => {
+    if (svgRef.current) {
+      const q = gsap.utils.selector(svgRef.current);
+      animationRef.current = gsap.to(q('.scanner'), {
+        opacity: 1,
+        y: 80,
+        duration: 1,
+        repeat: -1,
+        yoyo: true,
+        ease: 'power1.inOut'
+      });
+    }
+  };
+  const handleMouseLeave = () => {
+    // if (animationRef.current) {
+    // }
+  };
+
   return (
     <button
-      className={`text-black dark:text-white`}
+      className={`text-black dark:text-white dark:fill-white`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
       style={{ width: `${width}px`, height: `${height}px` }}>
-      <FingerPrintSvg />
+      <FingerPrintSvg ref={svgRef} />
     </button>
   );
 };
