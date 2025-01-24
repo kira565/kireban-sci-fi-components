a6225c9d98d252ee4785be7605d0be968e23d653
Kiriill Kagdin <rlyiamberserker@gmail.com>
2025-01-24 15:34:20 +0200

    dsa

--- lib/main.ts
+++ lib/main.ts
@@ -9,3 +9,7 @@ export { EvaTilePanel } from './components/EvaTilePanel/EvaTilePanel';
 export { EvaOptionList } from './components/EvaOptionList/EvaOptionList';
 export { GeometricLayout } from './components/GeometricLayout/GeometricLayout';
 export { Card } from './components/Card/Card';
+
+//@ts-ignore
+import * as all from '../dist/index.es.js';
+console.log(' Arara', all);
--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
       fileName: (format) => `index.${format}.js` // Format-specific file names.
     },
     rollupOptions: {
+      treeshake: false,
       onwarn(warning) {
         console.warn(warning);
       },
