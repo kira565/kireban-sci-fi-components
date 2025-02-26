8d0a0c946494cd1ed00a2de40ed67395474bbdbf
Kiriill Kagdin <rlyiamberserker@gmail.com>
2025-02-26 17:45:06 +0200

    da

--- lib/components/AboutPanel/AboutHeader.tsx
+++ lib/components/AboutPanel/AboutHeader.tsx
@@ -8,17 +8,48 @@ export interface AboutHeaderProps {
 export const AboutHeader: React.FC<AboutHeaderProps> = ({ text }) => {
   const container = useRef(null);
   const textRef = useRef(null);
+  const pointsRef = useRef(null);
 
   useEffect(() => {
     gsap.to(container.current, {
-      background: '#ffaa0048',
+      background: '#fa0',
       duration: 2
     });
   }, []);
 
   return (
-    <div ref={container} className="w-full text-2xl font-bold flex items-center p-2">
-      <span ref={textRef}>{text}</span>
+    <div className="d-flex">
+      <div ref={container} className="w-[1%] text-2xl font-bold flex items-center">
+        <span ref={textRef} className="opacity-0">
+          {text}
+        </span>
+        {/* <svg className="w-full h-full">
+          <g ref={pointsRef} id="points" transform="translate(430, 0)">
+            <rect x="0" y="0" width="15" height="15"></rect>
+            <rect x="0" y="30" width="15" height="15"></rect>
+            <rect x="0" y="60" width="15" height="15"></rect>
+            <rect x="0" y="90" width="15" height="15"></rect>
+
+            <rect x="15" y="15" width="15" height="15"></rect>
+            <rect x="15" y="45" width="15" height="15"></rect>
+            <rect x="15" y="75" width="15" height="15"></rect>
+
+            <rect x="30" y="0" width="15" height="15"></rect>
+            <rect x="30" y="30" width="15" height="15"></rect>
+            <rect x="30" y="60" width="15" height="15"></rect>
+            <rect x="30" y="90" width="15" height="15"></rect>
+
+            <rect x="45" y="15" width="15" height="15"></rect>
+            <rect x="45" y="45" width="15" height="15"></rect>
+            <rect x="45" y="75" width="15" height="15"></rect>
+
+            <rect x="60" y="0" width="15" height="15"></rect>
+            <rect x="60" y="30" width="15" height="15"></rect>
+            <rect x="60" y="60" width="15" height="15"></rect>
+            <rect x="60" y="90" width="15" height="15"></rect>
+          </g>
+        </svg> */}
+      </div>
     </div>
   );
 };
