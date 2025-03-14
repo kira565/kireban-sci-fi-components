2d49b02e4aee62bfd3272c61ad8b232396de395c
Kiriill Kagdin <rlyiamberserker@gmail.com>
2025-03-14 02:16:11 +0200

    fix

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,11 +2,11 @@ import gsap from 'gsap';
 
 export type AppearDirection = 'top' | 'left' | 'bottom' | 'right';
 
-export function appearFrom(ref: React.RefObject<HTMLElement | null>, direction: AppearDirection) {
+export function dissAppearTo(ref: React.RefObject<HTMLElement | null>, direction: AppearDirection) {
   if (!ref.current) return;
 
-  let coord: string = 'x';
-  let coordVal: string = '-100%';
+  let coord = 'x';
+  let coordVal = '0%';
 
   switch (direction) {
     case 'top':
@@ -27,19 +27,50 @@ export function appearFrom(ref: React.RefObject<HTMLElement | null>, direction: 
       coordVal = '-100%';
       break;
   }
+}
 
+export function appearFrom(
+  ref: React.RefObject<HTMLElement | null>,
+  direction: AppearDirection,
+  dissapear = false
+) {
+  if (!ref.current) return;
+
+  let coord: string = 'x';
+  let coordVal: string = dissapear ? '0%' : '-100%';
+
+  switch (direction) {
+    case 'top':
+      coord = 'y';
+      coordVal = '-100%';
+      break;
+    case 'bottom':
+      coord = 'y';
+      coordVal = '100%';
+      break;
+    case 'right':
+      coord = 'x';
+      coordVal = '100%';
+      break;
+    case 'left':
+    default:
+      coord = 'x';
+      coordVal = '-100%';
+      break;
+  }
+  //TODO HERE FUNCTION
   gsap.fromTo(
     ref.current,
     {
       [coord]: coordVal, // Move from this coordinate
-      opacity: 0,
+      opacity: dissapear ? 1 : 0,
       ease: 'power3.out',
       visibility: 'visible'
     },
     {
-      [coord]: '0%',
+      [coord]: dissapear ? coordVal : '0%',
       opacity: 1,
-      duration: 0.6,
+      duration: 1,
       ease: 'power3.out'
     }
   );
